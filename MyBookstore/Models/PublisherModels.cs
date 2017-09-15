8e18adddd0aa6570fdb6e37dd55d8d9bcae0caab
PatrickOfreneo <11606652@TAFT-CL309>
2017-09-15 14:17:56 +0800

    09/15/2017

---
+++ MyBookstore/Models/PublisherModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyBookstore.Models
+{
+    public class PublisherModels
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "Name")]
+        [MaxLength(40, ErrorMessage = "Up to 40 characters only!")]
+        [Required(ErrorMessage = "Required!")]
+        public string LastName { get; set; }
+
+    }
+}
